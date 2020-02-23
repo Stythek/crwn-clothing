b9edd86d75c6cc52a583cb1e228dad47e88b0963
Steffen Hytrek <steffen.hytrek@gmail.com>
2020-02-23 11:07:20 +0100

    #120 Fleshed out CheckoutPage w/ Selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -21,3 +21,11 @@ export const selectCartItemsCount = createSelector(
 			0
 		)
 );
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+	cartItems.reduce(
+		(accumalatedQuantity, cartItems) =>
+			accumalatedQuantity + cartItems.quantity * cartItems.price,
+		0
+	)
+);
