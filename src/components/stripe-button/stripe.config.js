0f18eedbdb48c6d184db4267e3623843d96090b9
Steffen Hytrek <steffen.hytrek@gmail.com>
2020-02-27 20:39:11 +0100

    #143 Removed configs from .gitignore for deployment

---
+++ src/components/stripe-button/stripe.config.js
@@ -0,0 +1,3 @@
+const publishableKeyForStripe = "pk_test_BcOdbjAfKUDi4Xn6yK3oHrsF00Yi7uRO17";
+
+export default publishableKeyForStripe;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+	apiKey: "AIzaSyAhRdDzjbKc0J51hJAv70p3Fka8uLjzsMU",
+	authDomain: "crwn-db-1ffe4.firebaseapp.com",
+	databaseURL: "https://crwn-db-1ffe4.firebaseio.com",
+	projectId: "crwn-db-1ffe4",
+	storageBucket: "crwn-db-1ffe4.appspot.com",
+	messagingSenderId: "520149174686",
+	appId: "1:520149174686:web:d3f2d272788749cb79ee8f",
+	measurementId: "G-57VB77X5F3"
+};
+export default FIREBASE_CONFIG;
